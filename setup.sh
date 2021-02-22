c544de6fa02d365b6073cb5adf4dc7ded9204cbf
smilekomiya <vetwith@gmail.com>
2021-02-22 18:05:57 +0900

    setup.sh を少し丁寧に

--- setup.sh
+++ setup.sh
@@ -1,2 +1,28 @@
-ln -s $PWD/vimrc ~/.vimrc
-ln -s $PWD/.gitconfig ~/.gitconfig
+#!/bin/bash 
+set -eu
+
+GIT_CONFIG_PATH="${HOME}/.gitconfig"
+if [ -e $GIT_CONFIG_PATH ]; then
+  read -p "${GIT_CONFIG_PATH} file exists. Replace it ? (y/n)" YN_GIT_CONFIG_FILE
+  if [ "${YN_GIT_CONFIG_FILE}" == "y" ]; then
+    rm -rf $GIT_CONFIG_PATH
+    ln -s $PWD/.gitconfig $GIT_CONFIG_PATH
+  else
+     echo "${GIT_CONFIG_PATH} skipped"
+  fi  
+else
+  ln -s $PWD/.gitconfig $GIT_CONFIG_PATH
+fi
+
+VIMRC_PATH="${HOME}/.vimrc"
+if [ -e $VIMRC_PATH ]; then
+  read -p "${VIMRC_PATH} file exists. Replace it ? (y/n)" YN_GITRC_FILE
+  if [ "${YN_VIMRC_FILE}" == "y" ]; then
+    rm -rf $VIMRC_PATH
+    ln -s $PWD/.vimrc $VIMRC_PATH
+  else
+    echo "${VIMRC_PATH} skipped"
+  fi
+else
+  ln -s $PWD/.vimrc $VIMRC_PATH
+fi
